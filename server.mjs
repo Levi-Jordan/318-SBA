b6e482a7c857f1b549a05db1564f2e3284642c4b
Levi-Jordan <Levijordananderson@gmail.com>
2025-04-17 15:49:13 -0600

    next commit

--- server.mjs
+++ server.mjs
@@ -1,2 +1,12 @@
-import express from express
+import express from 'express'
 
+const app = express()
+const PORT = 3000 || 3001
+
+app.get('/', (req, res) => {
+    res.send('Test');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Connected on Port: ${PORT}`);
+});
