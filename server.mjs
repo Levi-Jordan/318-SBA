4a05b26f79c26434e1c99d2c4c8eaacbdc6b2bba
Levi-Jordan <Levijordananderson@gmail.com>
2025-04-21 21:36:07 -0600

    another other commit

--- server.mjs
+++ server.mjs
@@ -3,10 +3,17 @@ import express from 'express'
 const app = express()
 const PORT = 3000 || 3001
 
+let myMovies = [
+    "Movie 1",
+    "Movie 2"
+]
+
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.send('Test');
+    res.render('index', { title: 'Hey', message: 'Hello there!', movies: myMovies });
 })
 
 app.listen(PORT, () => {
     console.log(`Server Connected on Port: ${PORT}`);
-});
+}); 
